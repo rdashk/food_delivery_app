d112120cfebdbaab2e00f77f60c8c852332f5daa
Darya Razmanova <rdashak8@gmail.com>
2022-05-02 08:15:37 +0800

    Initial commit

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/home/food_page_body.dart';
+import 'package:food_delivery_app/widgets/big_text.dart';
+import 'package:food_delivery_app/widgets/small_text.dart';
+
+import '../utils/colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // showing the header
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Russia", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Irkutsk",
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          // showing the body
+          FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
